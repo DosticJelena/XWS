18792de438dbb5e19c07798925e740bdc93f72ea
DosticJelena <jelenadostic2@gmail.com>
2020-05-18 18:38:07 +0200

    added search-service

---
+++ backend/services/Search-Service/src/main/java/xws/SearchServiceApplication.java
@@ -0,0 +1,21 @@
+package xws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SearchServiceApplication {
+
+    @RequestMapping("/health")
+    public String home() {
+        return "Hello world [AUTH]";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchServiceApplication.class, args);
+    }
+
+}
