f6cc19350646ef8e6d3ae85cb6009387f91305d1
NikolaRoncevic <nikola.roncevic123@gmail.com>
2020-05-19 21:21:18 +0200

    napravljeni osnovni modeli za cart service

---
+++ backend/services/Cart-Service/src/main/java/xws/model/RentingRequest.java
@@ -0,0 +1,32 @@
+package xws.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RentingRequest {
+
+    //privremeno samo da ima nesto
+    public enum Status {
+        PENDING,PAID
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private Status status;
+
+}
