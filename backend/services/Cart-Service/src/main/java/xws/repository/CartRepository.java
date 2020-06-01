8de59ab979b5a3c4da73a3682841d9856858ed16
NikolaRoncevic <nikola.roncevic123@gmail.com>
2020-06-01 19:26:04 +0200

    dodavanje-vozila-u-korpu

---
+++ backend/services/Cart-Service/src/main/java/xws/repository/CartRepository.java
@@ -0,0 +1,13 @@
+package xws.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import xws.model.Cart;
+
+import java.util.Optional;
+
+public interface CartRepository extends JpaRepository<Cart,Long> {
+
+    Optional<Cart> findOneById(Long id);
+
+    Optional<Cart> findOneByUserId(Long id);
+}
--- backend/services/Cart-Service/src/main/resources/data.sql
+++ backend/services/Cart-Service/src/main/resources/data.sql
@@ -1,1 +1,7 @@
 insert into cart (user_id) values (1);
+
+insert into vehicle(owner_id) values (1);
+insert into vehicle(owner_id) values (1);
+
+insert into cart_vehicle(cart_id,vehicle_id) values (1,1);
+insert into cart_vehicle(cart_id,vehicle_id) values (1,2);
