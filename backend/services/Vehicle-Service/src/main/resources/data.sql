ebcf597f6f507e226ed7e24daf82a47b12e46cbd
DosticJelena <jelenadostic2@gmail.com>
2020-06-03 13:20:38 +0200

    popravljeni podaci

--- backend/services/Vehicle-Service/src/main/resources/data.sql
+++ backend/services/Vehicle-Service/src/main/resources/data.sql
@@ -1,6 +1,7 @@
-INSERT INTO public.vehicle(cdwstatus, additional_price_per_km, brand, children_seats, distance, distance_per_rent, distance_per_rent_status, fuel_type, location, model, price, transmission, type)
-	VALUES ('da', 10, 'NAUDI', 1,100, 10, 'NE', 'ben', 'NS', '123', 100, 'manual', 'abc');
-INSERT INTO public.vehicle(cdwstatus, additional_price_per_km, brand, children_seats, distance, distance_per_rent, distance_per_rent_status, fuel_type, location, model, price, transmission, type)
-	VALUES ('da', 10, 'abc', 1,100, 10, 'NE', 'ben', 'BG', '123', 100, 'manual', 'abc');
-INSERT INTO public.vehicle(cdwstatus, additional_price_per_km, brand, children_seats, distance, distance_per_rent, distance_per_rent_status, fuel_type, location, model, price, transmission, type)
-	VALUES ('da', 10, '', 1,100, 10, 'NE', 'ben', 'NS', '123', 100, 'manual', 'abc');
+INSERT INTO public.vehicle(user_id, cdwstatus, additional_price_per_km, brand, children_seats, distance, distance_per_rent, distance_per_rent_status, fuel_type, location, model, price, transmission, vehicle_type)
+	VALUES (1, 'Yes', 10, 'NAUDI', 1,100, 0, 'No', 'ben', 'Novi Sad', '123', 100, 'Manual', 'A');
+INSERT INTO public.vehicle(user_id, cdwstatus, additional_price_per_km, brand, children_seats, distance, distance_per_rent, distance_per_rent_status, fuel_type, location, model, price, transmission, vehicle_type)
+	VALUES (1, 'Yes', 10, 'abc', 1,100, 10, 'Yes', 'ben', 'Beograd', '124', 150, 'Manual', 'B');
+INSERT INTO public.vehicle(user_id, cdwstatus, additional_price_per_km, brand, children_seats, distance, distance_per_rent, distance_per_rent_status, fuel_type, location, model, price, transmission, vehicle_type)
+	VALUES (1, 'No', 10, '', 1,100, 20, 'Yes', 'ben', 'Novi Sad', '125', 130, 'Manual', 'C');
+
