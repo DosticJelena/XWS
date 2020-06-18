d5d66d9d14fb07f383026c7c0166621a542a55d6
Mraseta <marko.raseta97@gmail.com>
2020-06-18 17:14:05 +0200

    new vehicle on agent app done

---
+++ agent-backend/src/main/java/xml/model/Picture.java
@@ -0,0 +1,30 @@
+package xml.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Picture {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonManagedReference
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Vehicle vehicle;
+
+    @Column
+    private String url;
+
+}
