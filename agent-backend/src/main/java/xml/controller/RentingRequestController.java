b5a036719aa9575c6a014c35d24354de9e84ed65
Mraseta <marko.raseta97@gmail.com>
2020-06-21 20:05:25 +0200

    agent back done

--- agent-backend/src/main/java/xml/controller/RentingRequestController.java
+++ agent-backend/src/main/java/xml/controller/RentingRequestController.java
@@ -11,6 +11,11 @@ import xml.model.RentingRequestVehicle;
 import xml.model.Vehicle;
 import xml.repository.RentingRequestVehicleRepository;
 import xml.service.RentingRequestService;
+import xml.service.VehicleService;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/rentingRequest")
@@ -19,11 +24,38 @@ public class RentingRequestController {
     @Autowired
     private RentingRequestVehicleRepository rentingRequestVehicleRepository;
 
+    @Autowired
+    private VehicleService vehicleService;
+
     @RequestMapping(value = "/report", method = RequestMethod.PUT, consumes = "application/json", produces = "application/json")
     public RentingRequestVehicle report(@RequestBody ReportRequestDTO request) {
         RentingRequestVehicle rrv = rentingRequestVehicleRepository.findOneByVehicleIdAndRentingRequestId(request.getVId(), request.getRId());
         rrv.setDistance(request.getDistance());
         rrv.setAdditionalInfo(request.getAdditionalInfo());
         return rentingRequestVehicleRepository.save(rrv);
+    }
+
+    @RequestMapping(value = "/finished", method = RequestMethod.GET, produces = "application/json")
+    public List<RentingRequestVehicle> getFinished(@RequestBody Id id) {
+        List<Vehicle> vehicles = vehicleService.findAllByOwner_id(id.id);
+
+        ArrayList<List<RentingRequestVehicle>> lista = new ArrayList<List<RentingRequestVehicle>>();
+        for(Vehicle v : vehicles) {
+            lista.add(rentingRequestVehicleRepository.findAllByVehicleIdAndEndDateIsLessThanAndAdditionalInfoIsNull(v.getId(), LocalDateTime.now()));
+        }
+
+        List<RentingRequestVehicle> ret = new ArrayList<>();
+        for(int i=0;i<lista.size();i++) {
+            List<RentingRequestVehicle> a = lista.get(i);
+            for(RentingRequestVehicle rrv : a) {
+                ret.add(rrv);
+            }
+        }
+
+        return ret;
+    }
+
+    public static class Id {
+        public Long id;
     }
 }
