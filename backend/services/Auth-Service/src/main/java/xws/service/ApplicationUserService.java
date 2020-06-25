4d590ef6b9c78024d5a582f2d52845a9726a1e63
DosticJelena <jelenadostic2@gmail.com>
2020-06-25 08:32:27 +0200

    user login and registration

--- backend/services/Auth-Service/src/main/java/xws/service/ApplicationUserService.java
+++ backend/services/Auth-Service/src/main/java/xws/service/ApplicationUserService.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import xws.dto.RegisterDTO;
 import xws.model.ApplicationUser;
+import xws.model.Person;
 import xws.repository.ApplicationUserRepository;
 
 import java.util.List;
@@ -18,6 +20,22 @@ public class ApplicationUserService {
     public List<ApplicationUser> findAll() { return applicationUserRepository.findAll(); }
 
     public ApplicationUser findOneById(Long id) { return applicationUserRepository.findOneById(id); }
+
+    public ApplicationUser findOneByUsername(String username) { return applicationUserRepository.findOneByUsername(username); }
+
+    public ApplicationUser save(RegisterDTO dto) {
+
+        Person person = new Person();
+        person.setStatus(ApplicationUser.Status.ACTIVE);
+        person.setRole(ApplicationUser.Role.PERSON);
+        person.setUsername(dto.getUsername());
+        person.setPassword(dto.getPassword());
+        person.setFirstName(dto.getFirstName());
+        person.setLastName(dto.getLastName());
+
+        return applicationUserRepository.save(person);
+
+    }
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public int updateUserStatus(ApplicationUser.Status status, Long id) {
