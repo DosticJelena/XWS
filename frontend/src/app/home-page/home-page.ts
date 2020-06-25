8e35c3b09d5670114dbb2943690d7393f0eb661b
DosticJelena <jelenadostic2@gmail.com>
2020-06-25 14:13:07 +0200

    login from home-page

--- frontend/src/app/home-page/home-page.ts
+++ frontend/src/app/home-page/home-page.ts
@@ -8,15 +8,22 @@ import { AuthService } from '../auth/auth.service';
 })
 export class HomePage {
 
-    userEmail:String;
-    userPassword:String;
+    userEmail: String;
+    userPassword: String;
 
-    constructor(public authService:AuthService){
+    constructor(public authService: AuthService) {
 
     }
 
-    loginRequest(formValues:any) {
-        this.authService.loginUser(formValues);
+    loginRequest(formValues: any) {
+        this.authService.loginUser(formValues)
+            .subscribe(
+                (data: any) => {
+                    console.log(data);
+                    this.authService.loggedUser = data;
+                    localStorage.setItem("loggedUser", JSON.stringify(data));
+                },
+                (error) => alert(error.text));
     }
 
 }
