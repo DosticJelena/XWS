bc01d0b97533d41e129b2b33bdebf092c1fcedec
NikolaRoncevic <nikola.roncevic123@gmail.com>
2020-07-08 18:27:17 +0200

    rade popusti

--- frontend/src/app/new-discount/new-discount.component.ts
+++ frontend/src/app/new-discount/new-discount.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DiscountService } from '../services/discount/discount.service';
 
 @Component({
   selector: 'app-new-discount',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewDiscountComponent implements OnInit {
 
-  constructor() { }
+  amount : number;
+
+  constructor(private discountService : DiscountService) { }
 
   ngOnInit(): void {
   }
+  submitNewAmmount() {
+    if(this.amount != undefined) {
+      this.discountService.addNewDiscount(this.amount).subscribe(
+        (data : any) => {
+            console.log(data);
+        },
+        (error: any) => {
+          console.log(error);
+        }
+      )
+  
+    }
+    }
 
 }
