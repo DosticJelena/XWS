02fc4c220f3ca380258355835def09d948e4a234
Mraseta <marko.raseta97@gmail.com>
2020-07-11 14:26:45 +0200

    minor changes

---
+++ agent-backend/src/main/java/xml/config/RabbitConfiguration.java
@@ -0,0 +1,42 @@
+package xml.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class RabbitConfiguration {
+    @Value("${fanout.exchange}")
+    private String fanoutExchange;
+    @Value("${queue.name}")
+    private String queueName;
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, true);
+    }
+    @Bean
+    FanoutExchange exchange() {
+        return new FanoutExchange(fanoutExchange);
+    }
+    @Bean
+    Binding binding(Queue queue, FanoutExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange);
+    }
+
+    @Bean
+    public CachingConnectionFactory rabbitConnectionFactory(RabbitProperties config) throws Exception {
+
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.getRabbitConnectionFactory().setUri("amqp://prxcjtzz:HtA1Z2wZcfHM41CzaEVkke4cxz9CSeJf@jellyfish.rmq.cloudamqp.com/prxcjtzz");
+
+        return connectionFactory;
+
+    }
+}
